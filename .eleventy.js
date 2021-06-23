c5f64be57b4cc4e7aeabd0e6a93ba107bbec3b42
Paul Applegate <paul@theapplegates.com>
2021-06-23 09:05:28 -0400

    adding 11ty-image to site

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,58 @@ const transforms = require('./utils/transforms.js')
 const shortcodes = require('./utils/shortcodes.js')
 const iconsprite = require('./utils/iconsprite.js')
 
+const Image = require("@11ty/eleventy-img")
+const path = require('path')
+
+
+async function imageShortcode(src, alt) {
+      let sizes = "(min-width: 1024px) 100vw, 50vw"
+      let srcPrefix = `./src/assets/images/`
+      src = srcPrefix + src
+      console.log(`Generating image(s) from:  ${src}`)
+      if(alt === undefined) {
+        // Throw an error on missing alt (alt="" works okay)
+        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+      }  
+      let metadata = await Image(src, {
+        widths: [150, 300,600, 900, 1500, 3000],
+        formats: ['avif', 'webp', 'jpeg'],
+        urlPath: "/images/",
+        outputDir: "./dist/images/",
+        /* =====
+        Now we'll make sure each resulting file's name will 
+        make sense to you. **This** is why you need 
+        that `path` statement mentioned earlier.
+        ===== */
+        filenameFormat: function (id, src, width, format, options) {
+          const extension = path.extname(src)
+          const name = path.basename(src, extension)
+          return `${name}-${width}w.${format}`
+        }
+      })  
+      let lowsrc = metadata.jpeg[0]  
+      return `<picture>
+        ${Object.values(metadata).map(imageFormat => {
+          return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+        }).join("\n")}
+        <img
+          src="${lowsrc.url}"
+          width="${lowsrc.width}"
+          height="${lowsrc.height}"
+          alt="${alt}"
+          loading="lazy"
+          decoding="async">
+      </picture>`
+    }
+    
+    
+
 module.exports = function (config) {
+    
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+    eleventyConfig.addLiquidShortcode("image", imageShortcode)
+      // === Liquid needed if `markdownTemplateEngine` **isn't** changed from Eleventy default
+    eleventyConfig.addJavaScriptFunction("image", imageShortcode)
     // Plugins
     config.addPlugin(pluginRss)
     config.addPlugin(pluginNavigation)
--- src/posts/2021-06-18-sample-post.md
+++ src/posts/2021-06-18-sample-post.md
@@ -5,3 +5,4 @@ excerpt: Some more lorem ipsum text
 
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto omnis, modi libero corrupti voluptates est vitae aspernatur, ipsum ea culpa nulla pariatur ullam temporibus architecto laudantium, soluta officia minus qui?
 
+{% image "volvo.jpg", "A Volvo XC60" %}
